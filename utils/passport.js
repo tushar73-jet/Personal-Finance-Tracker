e030fa6b4d4c95de7e12d2bc23d1d7640e35c31d
Tushar Kumar <184378329+tushar73-jet@users.noreply.github.com>
2026-05-01 13:01:17 +0530

    Add proxy: true to passport to fix http vs https redirect_uri on Render

--- utils/passport.js
+++ utils/passport.js
@@ -7,7 +7,8 @@ const { createDefaultCategories } = require('../utils/defaults');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL || "http://localhost:3000/api/auth/google/callback"
+    callbackURL: process.env.CALLBACK_URL || "http://localhost:3000/api/auth/google/callback",
+    proxy: true
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
